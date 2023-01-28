dee8433e59fafe096446ada0a3210b5010cd422e
Brock York <brock@polynubstudios.com>
2023-01-28 20:16:34 +1100

    Trying to get texture "component" working
    -Currently unable to add a texture from the sdl2 subsystem because
     of the reference not being static.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,19 @@ use std::fmt;
 use std::fmt::Display;
 use std::any::{TypeId, Any};
 use std::cell::{RefCell, RefMut, Ref};
+use std::path::Path;
+use std::time::Duration;
+
+//sdl2
+extern crate sdl2;
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::rect::Rect;
+use sdl2::rect::Point;
+
+//sdl2_image
+use sdl2::image::LoadTexture;
 
 struct Position {
 	x: f64,
@@ -44,11 +57,24 @@ struct Collidable {
 }
 
 struct Drawable {
-	//Image stuff here
+    x: i32,
+    y: i32,
+    w: u32,
+    h: u32,
+}
+
+impl Drawable {
+    pub fn new(x: i32, y: i32, w: u32, h: u32) -> Drawable {
+        return Drawable {x, y, w, h};
+    }
+}
+
+struct Graphics {
+    canvas: sdl2::render::Canvas<sdl2::video::Window>,
 }
 
 pub trait ComponentHashMap {
-	fn as_any(&self) -> &dyn Any;
+	fn as_any(&self) -> & dyn Any;
 	
 	fn as_any_mut(&mut self) -> &mut dyn Any;
 }
@@ -76,14 +102,14 @@ impl Display for Entity {
 
 struct EntitySystem {
 	next_id: u64,
-	entities: Vec<Entity>,
-	entity_names: HashMap<u64, String>,
+    entities: HashMap<u64, String>,
+	entity_names: HashMap<String, u64>,
 	components: HashMap<TypeId, Box<dyn ComponentHashMap>>,
 }
 
 impl EntitySystem {
 	pub fn new() -> EntitySystem {
-		return EntitySystem {next_id: 0, entities: Vec::new(), entity_names: HashMap::new(), components: HashMap::new()};
+		return EntitySystem {next_id: 0, entities: HashMap::new(), entity_names: HashMap::new(), components: HashMap::new()};
 	}
 
 	pub fn new_entity(&mut self) -> Result<Entity, String> {
@@ -98,33 +124,28 @@ impl EntitySystem {
 		let ent = Entity {id: self.next_id};
 
 		self.next_id += 1;
-		self.entities.push(ent);
-		self.entity_names.insert(ent.id, name);
+		self.entity_names.insert(name.clone(), ent.id);
+        self.entities.insert(ent.id, name);
 
 		return Ok(ent);
 	}
 
-	pub fn remove_entity(&mut self, ent: Entity) {
-		let mut found = false;
-		let mut index = 0;
+	pub fn remove_entity(&mut self, ent: Entity) -> Result<(), String> {
+        let name = match self.entities.remove(&ent.id) {
+            Some(n) => n,
+            None => return Err(format!("No such entity with id:{}", ent.id)),
+        };
 
-		for (i, e) in self.entities.iter().enumerate() {
-			if *e == ent {
-				index = i;
-				found = true;
-			}
-		}
-
-		if found {
-			self.entities.remove(index);
-		} else {
-			panic!("Failed to find entity for removal:{:?}", ent);
-		}
+		match self.entity_names.remove(&name) {
+            Some(_) => (),
+            None => return Err(format!("No such entity name {} but the entity id did exist", name)),
+        }
 
 		panic!("Removing components not implemented");
 	}
 
-/* Currently not working and I'm too tired to fix it
+//Currently not working and I'm too tired to fix it
+    /*
 	pub fn borrow_component_for_entity<ComponentType: 'static>(&self, entity: Entity) -> Result<&ComponentType, String> {
 		let component_hashmap = match self.components.get(&TypeId::of::<ComponentType>()) {
 			Some(chm) => chm,
@@ -141,7 +162,7 @@ impl EntitySystem {
 
 		return Err(format!("Unable to downcast ref to expected component type"));
 	}
-*/
+    */
 
 	pub fn borrow_all_components_of_type<ComponentType: 'static>(&self) -> Result<Ref<HashMap<u64, ComponentType>>, String> {
 		let component_hashmap = match self.components.get(&TypeId::of::<ComponentType>()) {
@@ -177,7 +198,7 @@ impl EntitySystem {
 			self.components.insert(TypeId::of::<ComponentType>(), Box::new(RefCell::new(HashMap::<u64, ComponentType>::new())));
 		}
 
-		let mut component_hashmap = match self.components.get_mut(&TypeId::of::<ComponentType>()) {
+		let component_hashmap = match self.components.get_mut(&TypeId::of::<ComponentType>()) {
 			Some(chm) => chm,
 			None => return Err(format!("Unknown component in store")),
 		};
@@ -188,6 +209,15 @@ impl EntitySystem {
 
 		return Ok(());
 	}
+
+    pub fn get_entity_for_name(&self, name: String) -> Result<Entity, String> {
+        let id = match self.entity_names.get(&name) {
+            Some(i) => i,
+            None => return Err(format!("No such entity with name:{}", name)),
+        };
+
+        return Ok(Entity {id: *id});
+    }
 
 /*
 	pub fn remove_component_from_entity<ComponentType: 'static>(&mut self, ent: Entity) -> Result<(), String> {
@@ -204,7 +234,7 @@ impl EntitySystem {
 */
 }
 
-fn System_Moveable(es: &mut EntitySystem, dt: f64) -> Result<(), String> {
+fn system_moveable(es: &mut EntitySystem, dt: f64) -> Result<(), String> {
 	let moveables = match es.borrow_all_components_of_type::<Moveable>() {
 		Ok(m) => m,
 		Err(e) => return Err(e),
@@ -228,10 +258,83 @@ fn System_Moveable(es: &mut EntitySystem, dt: f64) -> Result<(), String> {
 
 	return Ok(());
 }
+//fn system_drawable(es: &EntitySystem, canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, game_texture: &sdl2::render::Texture) -> Result<(), String> {
+fn system_drawable(es: &mut EntitySystem, dt: f64) -> Result<(), String> {
+    //Draw stuff here
+    let mut graphics_map = match es.borrow_all_components_of_type_mut::<Graphics>() {
+        Ok(g) => g,
+        Err(e) => return Err(e),
+    };
 
-fn main() {
-	let mut es = EntitySystem::new();
+    if graphics_map.len() != 1 {
+        return Err(format!("Expected exactly one graphics component got:{}", graphics_map.len()));
+    }
 
+    let graphics_ent = match es.get_entity_for_name(String::from("graphics")) {
+        Ok(ge) => ge,
+        Err(e) => return Err(e),
+    };
+
+    let mut graphics = match graphics_map.get_mut(&graphics_ent.id) {
+        Some(g) => g,
+        None => return Err(format!("Failed to find component for graphics entity")),
+    };
+
+    let mut canvas = &mut graphics.canvas;
+
+    let texture_map = match es.borrow_all_components_of_type::<sdl2::render::Texture>() {
+        Ok(tm) => tm,
+        Err(e) => return Err(e),
+    };
+
+    if texture_map.len() != 1 {
+        return Err(format!("Expected exactly one texture component got:{}", texture_map.len()));
+    }
+
+    let texture_ent = match es.get_entity_for_name(String::from("texture")) {
+        Ok(te) => te,
+        Err(e) => return Err(e),
+    };
+
+    let game_texture = match texture_map.get(&texture_ent.id) {
+        Some(t) => t,
+        None => return Err(format!("Failed to find component for texture entity")),
+    };
+
+	let drawables = match es.borrow_all_components_of_type::<Drawable>() {
+		Ok(d) => d,
+		Err(e) => return Err(e),
+	};
+
+	let mut positions = match es.borrow_all_components_of_type::<Position>() {
+		Ok(p) => p,
+		Err(e) => return Err(e),
+	};
+
+    canvas.clear();
+
+	for (id, drawable) in drawables.iter() {
+		match positions.get(&id) {
+			Some(position) => {
+                let center = Point::new(position.x as i32, position.y as i32);
+                /*
+                match canvas.copy(&game_texture, Some(Rect::new(drawable.x, drawable.y, drawable.w, drawable.h)), Some(Rect::from_center(center, drawable.w, drawable.h))) {
+                    Ok(_) => (),
+                    Err(e) => println!("Failed to copy texture:{}", e),
+                }
+                */
+			},
+			None => return Err(format!("No position for moveable for entity {}", id)),
+		}
+
+	}
+
+    canvas.present();
+
+    return Ok(());
+}
+
+fn create_player_entity(es: &mut EntitySystem) -> Result<Entity, String> {
 	let player = match es.new_entity_with_name("Player".to_string()) {
 		Ok(p) => {
 			println!("player:{}", p);
@@ -250,28 +353,161 @@ fn main() {
 		Err(e) => panic!("Failed to add moveable component to player:{}", e),
 	}
 
-	for i in 0..10 {
+	match es.add_component_to_entity(player, Drawable::new(17, 272, 15, 15)) {
+		Ok(_) => println!("Added Drawable component to player"),
+		Err(e) => panic!("Failed to add drawable component to player:{}", e),
+	}
+
+    return Ok(player);
+}
+
+fn main() {
+    //SDL2 setup
+    let sdl_context = match sdl2::init() {
+        Ok(sc) => sc,
+        Err(e) => {
+            println!("Failed to initialise SDL2:{}", e);
+            return;
+        },
+    };
+
+    let sdl_video = match sdl_context.video() {
+        Ok(sv) => sv,
+        Err(e) => {
+            println!("Failed to initialise SDL2_video subsystem:{}", e);
+            return;
+        },
+    };
+
+    let window = match sdl_video.window("Bomberus", 1280, 720)
+        .position_centered()
+        .build() {
+            Ok(w) => w,
+            Err(e) => {
+                println!("Failed to create SDL window:{}", e);
+                return;
+            },
+    };
+
+    let mut canvas = match window.into_canvas()
+        .accelerated()
+        .build() {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Failed to create hw accelerated canvas:{}", e);
+            return;
+        },
+    };
+
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    //SDL2_image setup
+    let sdl_image_context = match sdl2::image::init(sdl2::image::InitFlag::PNG) {
+        Ok(sic) => sic,
+        Err(e) => {
+            println!("Failed to initialise SDL2_image:{}", e);
+            return;
+        },
+    };
+
+    //Entity System setup
+	let mut es = EntitySystem::new();
+
+    //Load game texture
+    let texture_ent = match es.new_entity_with_name(String::from("game_texture")) {
+        Ok(te) => te,
+        Err(e) => {
+            println!("Failed to create texture entity:{}", e);
+            return;
+        },
+
+    };
+
+    let texture_creator_ent = match es.new_entity_with_name(String::from("texture_creator")) {
+        Ok(tc) => tc,
+        Err(e) => {
+            println!("Failed to create texture entity:{}", e);
+            return;
+        },
+
+    };
+
+    let texture_creator = canvas.texture_creator();
+
+    let game_texture = match texture_creator.load_texture(Path::new("assets/bomb_party_v4.png")) {
+        Ok(gt) => gt,
+        Err(e) => {
+            println!("Unable to load game texture:{}", e);
+            return;
+        },
+    };
+    //es.add_component_to_entity::<sdl2::render::Texture>(texture_ent, game_texture);
+
+    let player = match create_player_entity(&mut es) {
+        Ok(p) => p,
+        Err(e) => {
+            println!("Failed to create player:{}", e);
+            return;
+        },
+    };
+    let mut event_pump = match sdl_context.event_pump() {
+        Ok(ep) => ep,
+        Err(e) => {
+            println!("Failed to get event pump:{}", e);
+            return;
+        },
+    };
+
+    //Systems
+    let mut systems: Vec<&dyn Fn(&mut EntitySystem, f64) -> Result<(), String>> = vec![&system_moveable, &system_drawable];
+
+    let mut frame: usize = 0;
+    //Main game loop
+    'running: loop {
+        //Handle input
+        for event in event_pump.poll_iter() {
+            use sdl2::event::Event;
+            match event {
+                Event::KeyDown {keycode: Escape, ..} => {
+                    println!("Quiting");
+                    break 'running;
+                },
+                Event::Quit {..} => {
+                    println!("Quiting");
+                    break 'running;
+                },
+                _ => (),
+            }
+        }
+
+        //Game update
 		/* Need to fix this when I'm not so tired
 		match es.borrow_component_for_entity::<Position>(player) {
 			Ok(position) => println!("Frame:{} Player position x:{} y:{}", i, position.x, position.y),
 			Err(e) => println!("Failed to player position:{}", e),
 		}
-		*/
 		match es.borrow_all_components_of_type::<Position>() {
 			Ok(positions) => { 
 				match positions.get(&player.id) {
-					Some(position) => println!("Frame:{} Player position x:{} y:{}", i, position.x, position.y),
+					Some(position) => println!("Frame:{} Player position x:{} y:{}", frame, position.x, position.y),
 					None => println!("Failed to player position"),
 				}
 			},
 			Err(e) => println!("Unable to borrow positions:{}", e),
 		};
+		*/
 
+        //drawing
+        for system in systems.iter() {
+            system(&mut es, 1.0);
+        }
 
-		match System_Moveable(&mut es, 1.0) {
-			Ok(_) => println!("Ran System_Moveable"),
-			Err(e) => println!("Failed to run System_Moveable:{}", e),
-		}
-	}
+        //Sleep for the rest of the frame
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+
+        frame += 1;
+    }
 
 }
